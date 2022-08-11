c49fcd2608c5d51eb3d6f36d3e70241b8a702180
Quang <mnquang123@gmail.com>
2022-08-11 18:11:09 +0700

    add frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import ManualHeader from '../components/ManualHeader';
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Smart Contract Lottery</title>
+        <meta name="description" content="Our Smart Contract Lottery" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ManualHeader />
+      1234
+    </div>
+  );
+}
